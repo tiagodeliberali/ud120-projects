18224b3f6f486265927d0e742a22d4931e0ccf41
Tiago Deliberali Santos <tiago@vinya.com.br>
2017-03-09 23:00:31 -0300

    implementation of SVC

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -22,9 +22,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-#########################################################
-### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
-#########################################################
+clf = SVC(kernel='rbf', C=10000)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+prediction = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+accuracy = accuracy_score(labels_test, prediction)
+
+print 'Accuracy %s' % accuracy
 
