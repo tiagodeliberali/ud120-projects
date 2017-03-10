d1642ba918172026c791dfd92b00fe327db93099
Tiago Deliberali Santos <tiago@vinya.com.br>
2017-03-09 23:12:16 -0300

    added decision tre to the list of generated images

--- _drawer/Classifiers.py
+++ _drawer/Classifiers.py
@@ -1,5 +1,6 @@
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 def classifyGaussianNB(features_train, labels_train):   
     ### import the sklearn module for GaussianNB
@@ -19,6 +20,17 @@ def classifySVC(features_train, labels_train, kernel_name, c_value):
     ### return the fit classifier
     
     clf = SVC(kernel=kernel_name, C=c_value)
+    clf_fit = clf.fit(features_train, labels_train)
+    
+    return clf_fit
+	
+def classifyDecisionTree(features_train, labels_train, samples):
+    ### import the sklearn module for GaussianNB
+    ### create classifier
+    ### fit the classifier on the training features and labels
+    ### return the fit classifier
+    
+    clf = DecisionTreeClassifier(min_samples_split=samples)
     clf_fit = clf.fit(features_train, labels_train)
     
     return clf_fit
--- _drawer/studentMain.py
+++ _drawer/studentMain.py
@@ -10,8 +10,7 @@
 
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture, output_image
-from Classifiers import classifyGaussianNB
-from Classifiers import classifySVC
+import Classifiers as cl
 
 import numpy as np
 import pylab as pl
@@ -32,15 +31,15 @@ grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if l
 bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 
 
-printData("GaussianNB.png", classifyGaussianNB(features_train, labels_train))
+#printData("GaussianNB.png", cl.classifyGaussianNB(features_train, labels_train))
 
-printData("classifySVC_rbf_1.png", classifySVC(features_train, labels_train, 'rbf', 1.0))
-printData("classifySVC_rbf_1000.png", classifySVC(features_train, labels_train, 'rbf', 1000.0))
+#printData("classifySVC_rbf_1.png", cl.classifySVC(features_train, labels_train, 'rbf', 1.0))
+#printData("classifySVC_rbf_1000.png", cl.classifySVC(features_train, labels_train, 'rbf', 1000.0))
 
-printData("classifySVC_linear_1.png", classifySVC(features_train, labels_train, 'linear', 1.0))
-printData("classifySVC_linear_1000.png", classifySVC(features_train, labels_train, 'linear', 1000.0))
+#printData("classifySVC_linear_1.png", cl.classifySVC(features_train, labels_train, 'linear', 1.0))
+#printData("classifySVC_linear_1000.png", cl.classifySVC(features_train, labels_train, 'linear', 1000.0))
 
-
+printData("classifyDecisionTree_40.png", cl.classifyDecisionTree(features_train, labels_train, 40))
 
 
 
